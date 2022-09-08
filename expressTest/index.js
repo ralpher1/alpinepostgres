605001ff282652f8029c1bdaa04b8e90af443e17
ralph <rsdipietro@gmail.com>
2022-09-08 18:07:08 -0400

    added express

---
+++ expressTest/index.js
@@ -0,0 +1,31 @@
+var exp=require("express");
+const app=exp();
+const port=3000;
+
+app.use("/get",(req,res,next)=>{console.log("req"),next()});
+
+app.use("/",(req,res,next)=>{console.log("req1"),next()});
+
+app.use(exp.static('../build'));
+
+
+app.use("/",(req,res,next)=>{console.log("Shouldnt run");next()});
+app.get("/",(req,res,next)=>{console.log("Shouldnt run2");next()});
+app.get("/get",(req,res,next)=>{
+
+	console.log("get");
+	//res.local('wtf1',"1");
+	res.send("blah");
+	var x=1;
+	next();
+
+});
+
+
+app.use("/get",(req,res,next)=>{console.log("RESPONSE");
+	//res.location('/foo/bar');
+	next()});
+
+app.listen(port,()=>{console.log(`Listening on Port ${port} !`)});
+
+console.log("wtf")
